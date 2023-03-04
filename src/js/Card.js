86ec564431bfc30210b8078f4c64847ee04e6c5c
MariellaNik <118596924+MariellaNik@users.noreply.github.com>
2023-03-04 14:05:37 +0200

    Update Card.js

--- src/js/Card.js
+++ src/js/Card.js
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 import EventEmitter from "eventemitter3";
+import { formatCurrency } from "./utils";
 
 export default class Card extends EventEmitter {
   static get events() {
@@ -28,12 +29,12 @@ export default class Card extends EventEmitter {
 
   render() {
     const template = `
-<div class="card type-${this._type} ${classNames({
-      "is-danger": this._type === Card.types.HAWAIIAN,
-    })}">
-  <div class="emoji">🍕</div>
-  <span class="type">${this._type}</span>
-</div>
+      <div class="card type-${this._type} ${classNames({
+        "is-danger": this._type === Card.types.HAWAIIAN,
+      })}">
+        <div class="emoji">🍕</div>
+        <span class="type">${this._type}</span>
+      </div>
     `;
 
     this.container.innerHTML = template;
@@ -42,6 +43,32 @@ export default class Card extends EventEmitter {
         type: this._type,
         price: this._price,
       });
+      this.showNotification();
+    });
+  }
+
+  showNotification() {
+    const notificationContainer = document.createElement("div");
+    notificationContainer.classList.add("notification-container");
+
+    const notificationTemplate = `
+      <div class="notification ${classNames({
+        "is-danger": this._type === Card.types.HAWAIIAN,
+      })}">
+        <button class="delete"></button>
+        <div class="notification-content">
+          <span class="type">${this._type}</span>
+          <span class="price">${formatCurrency(this._price)}</span>
+        </div>
+      </div>
+    `;
+
+    notificationContainer.innerHTML = notificationTemplate;
+    document.body.appendChild(notificationContainer);
+
+    const closeButton = notificationContainer.querySelector(".delete");
+    closeButton.addEventListener("click", () => {
+      notificationContainer.remove();
     });
   }
 }
